207b6f309097fe9460951a052b36f8beb271aeb7
xxSumi <121988780+xxSumi@users.noreply.github.com>
2023-01-09 10:54:03 +0900

    Update upload.js

--- upload.js
+++ upload.js
@@ -13,7 +13,7 @@ function fileUpload(file) {
         .then(loadImage)
         .then(resize)
         .then(blob => {
-            return Promise.all([showPreview(blob), fetch("https://github.com/xxSumi/uploader/blob/main.git", { method: "POST", body: blob })]);
+            return Promise.all([showPreview(blob), fetch("https://github.com/xxSumi/uploader.git", { method: "POST", body: blob })]);
         })
         .then(showResult)
         .catch(showResult);
